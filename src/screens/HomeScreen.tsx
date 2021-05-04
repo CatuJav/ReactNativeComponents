f737d15d9dada8100ca0d802767f27ff4b5f45af
Javier <jalcoser1996@gmail.com>
2021-05-03 22:23:41 -0500

    Instalación y configuración de iconos

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,10 +1,12 @@
 import React from 'react'
+import Icon from 'react-native-vector-icons/Ionicons';
 import { Text, View } from 'react-native';
 
 export const HomeScreen = () => {
     return (
         <View>
             <Text>Home Screen</Text>
+            <Icon name='star-outline' size={30} color='blue'/>
         </View>
     )
 }
