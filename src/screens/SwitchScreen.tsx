e41055577fd2d2550da1448d2b6db5856575b3c1
Javier <jalcoser1996@gmail.com>
2021-05-08 21:11:33 -0500

    Uso de multiples Switches

--- src/screens/SwitchScreen.tsx
+++ src/screens/SwitchScreen.tsx
@@ -29,6 +29,14 @@ export const SwitchScreen = () => {
                 <Text style={styles.switchText}>isActived</Text>
                 <CustomSwitch isOn={isActive} onChange={(value)=>{onChange(value,'isActive')}}/>
             </View>
+            <View style={styles.switchRow}>
+                <Text style={styles.switchText}>isHungry</Text>
+                <CustomSwitch isOn={isHungry} onChange={(value)=>{onChange(value,'isHungry')}}/>
+            </View>
+            <View style={styles.switchRow}>
+                <Text style={styles.switchText}>isHappy</Text>
+                <CustomSwitch isOn={isHappy} onChange={(value)=>{onChange(value,'isHappy')}}/>
+            </View>
             <Text style={styles.switchText}>{JSON.stringify(state,null,3)}</Text>
         </View>
     )
@@ -38,7 +46,8 @@ const styles = StyleSheet.create({
     switchRow:{
         flexDirection:'row',
         justifyContent:'space-between',
-        alignItems:'center'
+        alignItems:'center',
+        marginVertical:10,
     },
     switchText:{
         fontSize:25
