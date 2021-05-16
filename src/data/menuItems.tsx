cb960d1e2c93cfbbe67e28f3457f7379cefc915c
Javier <jalcoser1996@gmail.com>
2021-05-15 21:31:43 -0500

    Agrego nueva pantalla SlidesScreen

--- src/data/menuItems.tsx
+++ src/data/menuItems.tsx
@@ -46,5 +46,10 @@ export const menuItems:MenuItem[]=[
         icon:'download-outline',
         component:'InfiniteScrollScreen'
     },
+    {
+        name:'Slides',
+        icon:'flower-outline',
+        component:'SlidesScreen'
+    },
     
 ]
