5259c9d16e5978b3ee8bf9fd5b7452f10a667570
Javier <jalcoser1996@gmail.com>
2021-05-17 22:38:30 -0500

    Puntos indicativos
    Muestra puntos en el cual se encuentra las imágenes y los puntos

--- src/screens/SlidesScreen.tsx
+++ src/screens/SlidesScreen.tsx
@@ -1,7 +1,7 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { ImageSourcePropType, SafeAreaView, View, Text, Dimensions, Image, StyleSheet } from 'react-native';
 
-import Carousel from 'react-native-snap-carousel';
+import Carousel, {Pagination} from 'react-native-snap-carousel';
 
 
 const {height:screenHeight,width:screenWidth}=Dimensions.get('window')
@@ -31,6 +31,7 @@ const items: Slide[] = [
 
 export const SlidesScreen = () => {
 
+    const [activeIndex, setActiveIndex] = useState(0);
     const renderItem=(items:Slide)=>{
         return (<View style={{
                         flex:1,
@@ -63,9 +64,24 @@ export const SlidesScreen = () => {
               //ref={(c) => { this._carousel = c; }}
               data={items}
               renderItem={({item}:any)=> renderItem(item)}
-              sliderWidth={screenHeight}
+              sliderWidth={screenWidth}
               itemWidth={screenWidth}
               layout='default'
+              //Para obtener el index del elemento
+              onSnapToItem={(index)=>{
+                setActiveIndex(index);
+              }}
+            />
+            <Pagination
+                dotsLength={items.length}
+                activeDotIndex={activeIndex}
+                //dotColor='red'
+                dotStyle={{
+                    width:10,
+                    height:10,
+                    borderRadius:10,
+                    backgroundColor:'#5856D6'
+                }}
             />
         </SafeAreaView>
     )
